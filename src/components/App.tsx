e739b843a667c6c2ca06adda0ed52e5c8821c80e
EwaDwornik <ewaannadwornik@gmail.com>
2022-08-10 20:05:44 +0000

    adding deadline and sorting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,40 +5,49 @@ import NewTask, {Task, tasksDB} from '../components/NewTask';
 
 function App() {
     const [list, updateList] = useState(tasksDB);
-
     const addTask = (task: Task) => {
-        updateList([...list, task])
+        const newTask: Task[] = [...list, task];
+        newTask.sort((a, b) => (b.deadline < a.deadline) ? 1 : -1)
+        updateList(newTask)
     }
 
-    const handleChange = (event: any, task: Task) => {
-        if (event.target.checked) {
-            task.complete = false;
-        } else {
-            task.complete = true;
-        }
-
-    };
-
-    const taskCards: any[] = [];
-    for (let task of tasksDB) {
-        taskCards.push(
-            <div className="card">
-                <div className="card-body row">
-                    <div className="col-1">{task.text}</div>
-                    <div className="col-1">
-                        <h3 style={{ textDecoration: task.complete ? 'line-through' : undefined }}>{task.text}</h3>
-                        <input type="checkbox" checked={task.complete} />
-                    </div>
-                </div>
-            </div>
-        )
+    const toggleTask = (index: number) => {
+        const newTask: Task[] = [...list];
+        newTask[index].complete = !newTask[index].complete;
+        updateList(newTask);
     }
+
+    const handleDelete = (index: number) => {
+        const newTasks: Task[] = [...list];
+        newTasks.splice(index, 1);
+        updateList(newTasks);
+    }
+
+    const today = new Date ();
 
     return (
         <div className="row d-flex justify-content-center mt-5">
             <div className='col-md-6'>
                 <NewTask props={addTask}/>
-                {taskCards}
+                {
+                    list.map((task: Task, index: number) => (
+                        <div className="card">
+                            <div className="row" key={index}>
+                                <div className="col-md-auto my-auto"><input type="checkbox" onChange={() => toggleTask(index)}/></div>
+                                <div className="col my-auto"
+                                    style={{textDecoration: task.complete ? "line-through" : "none"}}>{task.text}
+                                </div>
+                                <div className="col col-lg-4 my-auto" style={{color: (today.toISOString().split('T')[0] > task.deadline) ? 'red' : 'black'}}>
+                                    {task.deadline}
+                                </div>
+                                <div className="col col-lg-2 h-100">
+                                    <button className="btn btn-primary form-control h-100"
+                                            onClick={() => handleDelete(index)}>Delete
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
             </div>
         </div>
     )
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,53 +1,70 @@
 import React, {useState} from 'react';
 import "../style/App.css";
 
-function generateId() {
-    return nextId++;
-}
-
 export interface Task {
-    id: number;
     text: string;
     complete: boolean;
+    deadline: string;
 }
-
-let nextId = 1;
 
 const initialState: Task = {
-    id: 1,
     text: '',
-    complete: false
+    complete: false,
+    deadline: ''
 }
 
-export const tasksDB: Task[] = [];
+export const tasksDB: Task[] = [
+    {
+        text: 'Push your project on GitHub',
+        complete: false,
+        deadline: '2022-08-12'
+    },
+    {
+        text: 'Write a cover letter',
+        complete: false,
+        deadline: '2022-08-22'
+    },
+];
 
 function NewTask({props}: any) {
     const [formState, setFormState] = useState(initialState);
 
     const handleSubmit = (event: any) => {
         event.preventDefault();
-        tasksDB.push({...formState, id: generateId()});
-        tasksDB.sort((a, b) => (b.id > a.id) ? 1 : -1)
+        tasksDB.push({...formState});
         props(formState);
         setFormState(initialState);
     }
 
     return (
         <div>
-            <form onSubmit={handleSubmit} >
-                <div>
-                    <label className="form-label">To-do list:</label>
-                    <input
-                        type='text'
-                        className="form-control"
-                        placeholder="type a task"
-                        required
-                        value={formState.text}
-                        onChange={e => {
-                            setFormState({...formState, text: e.target.value});
-                        }}/>
+            <form onSubmit={handleSubmit}>
+                <div className='row'>
+                    <div className="col-8 ">
+                        <label className="form-label">To-do list:</label>
+                        <input
+                            type='text'
+                            className="form-control"
+                            placeholder="type a task"
+                            required
+                            value={formState.text}
+                            onChange={e => {
+                                setFormState({...formState, text: e.target.value});
+                            }}/>
+                    </div>
+                    <div className="col-4">
+                        <label className="form-label">Deadline:</label>
+                        <input
+                            type='date'
+                            className="form-control"
+                            required
+                            value={formState.deadline}
+                            onChange={e => {
+                                setFormState({...formState, deadline: e.target.value});
+                            }}/>
+                    </div>
                 </div>
-                <div >
+                <div>
                     <button type="submit" className="btn btn-primary form-control">add</button>
                 </div>
             </form>
