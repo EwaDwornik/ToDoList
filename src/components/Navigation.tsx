74e3b91a026913de3ce68d00bb25032227f1a4db
EwaDwornik <ewaannadwornik@gmail.com>
2022-11-09 12:38:24 +0000

    final version, very easy to-do list.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import '../style/App.css';
 
 function Navigation() {
     return (
         <div className="navbar bg-light fixed-top">
             <div className="container-fluid">
-                <a className="navbar-brand" href="src/components/App#">To-do list & Sass practicing</a>
+                <a className="navbar-brand" href="src/components/App#">To-do list</a>
             </div>
         </div>
     );
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import "../style/App.css";
 
 export interface Task {
     text: string;
